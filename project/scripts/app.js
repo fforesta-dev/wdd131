2cb0154b375e4311a21c1d4113f2cea02d0248aa
fforesta-dev <f.foresta86@pm.me>
2025-10-09 16:45:56 +0200

    Some fixes

--- project/scripts/app.js
+++ project/scripts/app.js
@@ -1,3 +1,11 @@
+// Current year
+const cy = document.getElementById("currentyear");
+if (cy) cy.textContent = new Date().getFullYear();
+
+// Last modified
+const lm = document.getElementById("lastModified");
+if (lm) lm.textContent = `Last Modification: ${document.lastModified}`;
+
 // app.js
 import { getFavorites, toggleFavorite, getTheme, setTheme } from "./storage.js";
 import { NAMES } from "./names.js";
